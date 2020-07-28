f71ac2654bbe5fab5b08d8f6ddab95758e48206b
John Quarmor <johnequarmor@gmail.com>
2020-07-28 05:51:36 +0000

    Created components

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Content = function () {
+	return (
+		<div>
+			<p>
+				Body, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+				animi, veritatis quidem ullam fugiat, ad consequatur corrupti
+				voluptatibus, quibusdam voluptatem aliquam quia. Nostrum porro quam
+				possimus minus praesentium? Modi, enim?
+			</p>
+		</div>
+	);
+};
+
+export default Content;
