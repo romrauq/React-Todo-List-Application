b3aa8bff32916d06f7b98bb9d19a3209762b37f0
John Quarmor <johnequarmor@gmail.com>
2020-08-19 18:39:15 +0000

    Passed "markComplete" through Todos as prop.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,9 +4,14 @@ import PropTypes from "prop-types";
 
 class Todos extends Component {
 	// state = {};
+
 	render() {
 		return this.props.todos.map((todo) => (
-			<TodoItem key={todo.id} todo={todo} />
+			<TodoItem
+				key={todo.id}
+				todo={todo}
+				markComplete={this.props.markComplete}
+			/>
 		));
 	}
 }
