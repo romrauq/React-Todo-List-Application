0fef9e8ca4549ea8213a474f4d7e08f9e44ef1ad
John Quarmor <johnequarmor@gmail.com>
2020-08-20 17:44:10 +0000

    Imported & rendered Header to main App component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 
 class App extends Component {
@@ -45,6 +46,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="main-component">
+				<Header />
 				<Todos
 					todos={this.state.todos}
 					markComplete={this.markComplete}
