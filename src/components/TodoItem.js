35bbd4fc6943cf0b0bdfd68f6b5e41fc2925bd31
John Quarmor <johnequarmor@gmail.com>
2020-08-20 17:32:30 +0000

    Created button and passed a method as a prop.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,7 @@ class TodoItem extends Component {
 		return {
 			textDecoration: this.props.todo.completed ? "line-through" : "none",
 			fontSize: "2rem",
+			width: "250px",
 			padding: "5px",
 			marginBottom: "10px",
 			backgroundColor: "wheat",
@@ -24,6 +25,9 @@ class TodoItem extends Component {
 						onChange={this.props.markComplete.bind(this, id)}
 					/>{" "}
 					{title}
+					<button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>
+						&times;
+					</button>
 				</p>
 			</div>
 		);
@@ -33,6 +37,16 @@ class TodoItem extends Component {
 // Setting TodoItems' PropType:
 TodoItem.propTypes = {
 	todo: PropTypes.object.isRequired,
+};
+
+const btnStyle = {
+	color: "white",
+	padding: "5px 9px",
+	backgroundColor: "#ff0000",
+	border: "none",
+	borderRadius: "50%",
+	cursor: "pointer",
+	float: "right",
 };
 
 export default TodoItem;
