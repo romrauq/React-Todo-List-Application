0e289e6decdcb34a71400c4f1a0aa3cee2ab6377
John Quarmor <johnequarmor@gmail.com>
2020-08-22 10:02:09 +0000

    Created About component page.

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+			<p>Todo List App v1.0.0</p>
+		</React.Fragment>
+	);
+};
+
+export default About;
