d591c2301f5812abbd5bd2b4be81ec53ff9f23c0
John Quarmor <johnequarmor@gmail.com>
2020-08-22 10:37:13 +0000

    Styled About page component.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,19 @@ import React from "react";
 const About = () => {
 	return (
 		<React.Fragment>
-			<h1>About</h1>
-			<p>Todo List App v1.0.0</p>
+			<h1 style={aboutStyles}>About Application</h1>
+			<p style={textStyles}>Todo List App v1.0.0</p>
+			<p style={textStyles}>Created using React</p>
 		</React.Fragment>
 	);
+};
+
+const aboutStyles = {
+	textAlign: "center",
+};
+
+const textStyles = {
+	fontSize: "1.5rem",
 };
 
 export default About;
